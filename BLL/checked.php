4a82a6cee394ac7f3f0b4e0d6bb348d9fe78e37b
abdallah.sarayrah <abdallah.dev85@gmail.com>
2020-09-13 17:00:28 +0300

    save_checked

--- BLL/checked.php
+++ BLL/checked.php
@@ -7,6 +7,9 @@ class checked extends my_db {
     function save_checked($user_id, $location_id) {
         $query = "INSERT INTO `transaction` (`transaction_type`, `user_id`, `location_id`) "
                 . "VALUES (3, ?, ?)";
+        $datatypes = "iii";
+        $vars = array(&$user_id, &$location_id);
+        $this->mod_data($query, $datatypes, $vars);
     }
 
 }
