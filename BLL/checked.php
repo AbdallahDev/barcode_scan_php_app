50908fd0049720778ee72661229a31332d70772d
abdallah.sarayrah <abdallah.dev85@gmail.com>
2020-09-13 16:58:47 +0300

    save_checked

--- BLL/checked.php
+++ BLL/checked.php
@@ -2,4 +2,11 @@
 
 include_once '../DAL/my_db.php';
 
+class checked extends my_db {
 
+    function save_checked($user_id, $location_id) {
+        $query = "INSERT INTO `transaction` (`transaction_type`, `user_id`, `location_id`) "
+                . "VALUES (3, ?, ?)";
+    }
+
+}
