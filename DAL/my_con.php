107e2598441e53b63c4862689f5d4cf40dc167cc
abdallah.sarayrah <abdallah.dev85@gmail.com>
2020-09-13 16:34:38 +0300

    db connection

--- DAL/my_con.php
+++ DAL/my_con.php
@@ -3,7 +3,7 @@
 class my_con {
 
     public function my_con() {
-        $con = new mysqli('localhost', 'appDev', 'qf8MfbH0OxpxcxBz', 'barcodescan');
+        $con = new mysqli('localhost', 'appDev', 'l0nmePaB98cnzSji', 'barcodescan');
         mysqli_query($con, "SET NAMES 'utf8'");
         mysqli_query($con, 'SET CHARACTER SET utf8');
         return $con;
