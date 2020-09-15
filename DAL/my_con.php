8e2daa285d5345a5a612bc199de41fd4486975e0
abdallah.sarayrah <abdallah.dev85@gmail.com>
2020-09-15 23:34:36 +0300

    distributor

--- DAL/my_con.php
+++ DAL/my_con.php
@@ -3,7 +3,8 @@
 class my_con {
 
     public function my_con() {
-        $con = new mysqli('localhost', 'appDev', 'l0nmePaB98cnzSji', 'barcodescan');
+//        $con = new mysqli('localhost', 'appDev', 'l0nmePaB98cnzSji', 'barcodescan');
+        $con = new mysqli('sql12.freemysqlhosting.net', 'sql12365831', 'zQQUNfS1qF', 'sql12365831');
         mysqli_query($con, "SET NAMES 'utf8'");
         mysqli_query($con, 'SET CHARACTER SET utf8');
         return $con;
